6ab93e3c324fe9dd1b6cb799fbfdab4e2a217f14
Nick Yazdani <nnyazdan@deakin.edu.au>
2020-11-28 11:34:14 +1100

    scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ScrollToTop from './utilities/ScrollToTop';
 
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
@@ -8,13 +9,16 @@ import About from './pages/About/About';
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Fragment>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Fragment>
     </Router>
   );
-}
+};
 
 export default App;
